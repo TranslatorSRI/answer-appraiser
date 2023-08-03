fbffb73c7409f66b81126372a000dfe8d491ee61
Max Wang <max@covar.com>
2023-08-03 14:12:52 -0400

    Actually call novelty code

--- app/ordering_components.py
+++ app/ordering_components.py
@@ -6,6 +6,8 @@ from tqdm import tqdm
 from .config import settings
 from .clinical_evidence.compute_clinical_evidence import compute_clinical_evidence
 
+from .novelty.compute_novelty import compute_novelty
+
 redis_pool = redis.ConnectionPool(
     host=settings.redis_host,
     port=settings.redis_port,
@@ -42,15 +44,16 @@ def get_clinical_evidence(result, message, logger, db_conn):
     return compute_clinical_evidence(result, message, logger, db_conn)
 
 
-def get_novelty(result, message, logger):
-    # TODO get novelty from novelty package
-    return 0
+def get_novelty(message, logger):
+    return compute_novelty(message, logger)
 
 
 def get_ordering_components(message, logger):
     logger.debug(f"Computing scores for {len(message['results'])} results")
     db_conn = redis.Redis(connection_pool=redis_pool)
-    for result_index, result in enumerate(tqdm(message.get("results") or [])):
+    novelty_scores_dict = get_novelty(message, logger).to_dict(orient="index")
+    novelty_scores = {node["drug"]: node["novelty_score"] for node in novelty_scores_dict.values()}
+    for result in tqdm(message.get("results") or []):
         clinical_evidence_score = get_clinical_evidence(
             result,
             message,
@@ -62,8 +65,9 @@ def get_ordering_components(message, logger):
             "clinical_evidence": clinical_evidence_score,
             "novelty": 0,
         }
-        if result["ordering_components"]["clinical_evidence"] == 0:
+        if clinical_evidence_score == 0:
             # Only compute novelty if there is no clinical evidence
-            result["ordering_components"]["novelty"] = get_novelty(
-                result, message, logger
-            )
+            for node_bindings in result.get("node_bindings", {}).values():
+                for node_binding in node_bindings:
+                    if node_binding["id"] in novelty_scores:
+                        result["ordering_components"]["novelty"] = novelty_scores[node_binding["id"]]
